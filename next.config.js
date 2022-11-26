c1c15e3f5dbada36485cc6dff768e584eb4f65c0
Fyfy Dev <105049869+stream-protocol@users.noreply.github.com>
2022-11-26 10:10:17 +0200

    First commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true }};
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    return config;
+  },
+}
+
+module.exports = nextConfig
